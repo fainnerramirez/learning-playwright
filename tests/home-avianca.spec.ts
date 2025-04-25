2c2b592fb64b0b555510843eba7fed65d0702c59
fainnerramirez <fainnerramirez@gmail.com>
2025-04-25 16:17:17 -0500

    feat: add functions custom test playwright

--- tests/home-avianca.spec.ts
+++ tests/home-avianca.spec.ts
@@ -3,53 +3,10 @@ import { test } from "../utils/functions.utils";
 test.describe("Busqueda en newSite de avianca", () => {
     test('Abrir Home de Avianca', async ({ page }) => {
         await page.goto("https://www.avianca.com/");
-        //si existe la sessión de aceptación de Cookies
-        const elementCookie = await page.isElementPresent("#onetrust-accept-btn-handler");
-        console.log("elementCookie => ", elementCookie);
-
-        if (elementCookie) {
-            await page.waitForTimeout(2000); // espera 2 segundos
-            await page.click("#onetrust-accept-btn-handler");
-        }
-
-        // Rellenando los valores de la búsqueda
-
-        // Origen
+        await page.verifyCookies("#onetrust-accept-btn-handler");
         await page.selectOriginFlight("Mede");
-        // Destino
         await page.selectDestinationFlight("Bogo");
-
-        // //fecha de ida y de vuelta
-        // const isCheckIdaYVuelta = await page.locator("#journeytypeId_0").isChecked();
-
-        // if (isCheckIdaYVuelta) {
-
-        //     const ida = await page.locator("#departureDateButtonId");
-        //     await ida.click();
-        //     const dayWrapper = await page.locator(".ngb-dp-day:not(.disabled)");
-
-        //     const datesIda = await dayWrapper.locator(".custom-day_day");
-        //     await datesIda.first().click();
-
-        //     const vuelta = await page.locator("#arrivalInputDatePickerId");
-        //     await vuelta.click();
-
-        //     const datesVuelta = await dayWrapper.locator(".custom-day_day");
-        //     await datesVuelta.first().click();
-        // }
-        // else {
-
-        //     const ida = await page.locator("#departureDateButtonId");
-        //     await ida.click();
-        //     const dayWrapper = await page.locator(".ngb-dp-day:not(.disabled)");
-        //     const datesIda = await dayWrapper.locator(".custom-day_day");
-        //     await datesIda.first().click();
-        // }
-
-        // //buscar button
-        const buttonSearch = await page.locator("#searchButton");
-        await buttonSearch.click();
-
+        await page.selectButtonAndClick("#searchButton");
         await page.close();
     });
 });
--- utils/functions.utils.ts
+++ utils/functions.utils.ts
@@ -1,4 +1,4 @@
-import { test as base } from '@playwright/test';
+import { test as base, selectors } from '@playwright/test';
 
 export const test = base.extend({
   page: async ({ page }, use) => {
@@ -22,6 +22,53 @@ export const test = base.extend({
       await page.waitForTimeout(2000);
       await page.locator(".station-control-list_item_link").first().click();
     };
+
+    page.verifyCookies = async (selector: string) => {
+      const elementCookie = await page.isElementPresent(selector);
+      if (elementCookie) {
+        await page.waitForTimeout(2000);
+        await page.click(selector);
+      }
+    };
+
+    page.selectButtonAndClick = async (selector: string) => {
+      const buttonSearch = await page.locator(selector);
+      if(buttonSearch) await buttonSearch.click();
+    };
+
+    page.selectDateInitFlight = async (date?: Date) => {
+      throw Error("Sin Implementación");
+      // //fecha de ida y de vuelta
+        // const isCheckIdaYVuelta = await page.locator("#journeytypeId_0").isChecked();
+
+        // if (isCheckIdaYVuelta) {
+
+        //     const ida = await page.locator("#departureDateButtonId");
+        //     await ida.click();
+        //     const dayWrapper = await page.locator(".ngb-dp-day:not(.disabled)");
+
+        //     const datesIda = await dayWrapper.locator(".custom-day_day");
+        //     await datesIda.first().click();
+
+        //     const vuelta = await page.locator("#arrivalInputDatePickerId");
+        //     await vuelta.click();
+
+        //     const datesVuelta = await dayWrapper.locator(".custom-day_day");
+        //     await datesVuelta.first().click();
+        // }
+        // else {
+
+        //     const ida = await page.locator("#departureDateButtonId");
+        //     await ida.click();
+        //     const dayWrapper = await page.locator(".ngb-dp-day:not(.disabled)");
+        //     const datesIda = await dayWrapper.locator(".custom-day_day");
+        //     await datesIda.first().click();
+        // }
+    };
+
+    page.selectDateEndFlight = async (date?: Date) => {
+      throw Error("Sin Implementación");
+    }
 
     await use(page);
   }
