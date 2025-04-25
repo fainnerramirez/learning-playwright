8f6571f0e12ee12da97dffa3823de69255aad77c
fainnerramirez <fainnerramirez@gmail.com>
2025-04-25 17:48:05 -0500

    feat: update changes

--- tests/home-avianca.spec.ts
+++ tests/home-avianca.spec.ts
@@ -6,6 +6,8 @@ test.describe("Busqueda en newSite de avianca", () => {
         await page.verifyCookies("#onetrust-accept-btn-handler");
         await page.selectOriginFlight("Mede");
         await page.selectDestinationFlight("Bogo");
+        await page.selectDateInitFlight();
+        await page.selectDateEndFlight();
         await page.selectButtonAndClick("#searchButton");
         await page.close();
     });
