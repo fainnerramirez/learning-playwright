25cd479f590f38d76bba6ebb61eb6c4cd471e62c
fainnerramirez <fainnerramirez@gmail.com>
2025-04-28 16:21:59 -0500

    featL: update photo

--- tests/home-avianca.spec.ts
+++ tests/home-avianca.spec.ts
@@ -9,15 +9,15 @@ test.describe("Busqueda en newSite de avianca", () => {
         await page.waitForTimeout(3000);
 
         await page.selectOriginFlight("cali");
-        await page.setPhoto("campo-origen");
+        // await page.setPhoto("campo-origen");
         await page.selectDestinationFlight("Bogo");
-        await page.setPhoto("campo-destino");
+        // await page.setPhoto("campo-destino");
         await page.waitForTimeout(3000);
         await page.selectButtonAndClick("#searchButton");
-        await page.setPhoto("click-buscar-vuelos");
+        // await page.setPhoto("click-buscar-vuelos");
 
         await page.waitForTimeout(5000);
-        await page.setPhoto("finaliza-test");
+        // await page.setPhoto("finaliza-test");
         //await page.waitForSelector("#journeysContainerId_0", { timeout: 30000 });
     });
 });
