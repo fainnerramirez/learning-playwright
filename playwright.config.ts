bf1fc3cb30c4aa9c48bad052cd0e4b33c6334f25
fainnerramirez <fainnerramirez@gmail.com>
2025-04-28 11:38:41 -0500

    modified status headless to true

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
     testDir: './tests',
     timeout: 30000,
     use: {
-        headless: false,
+        headless: true,
         viewport: { width: 1280, height: 720 },
         ignoreHTTPSErrors: true,
         video: 'off',
